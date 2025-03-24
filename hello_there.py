b03afe905bc2853b23679b5a1619b691c3647705
Jonathan <jonathan@corll.org>
2025-03-24 18:27:49 -0400

    Hello there niner
    Hello there niner

---
+++ hello_there.py
@@ -0,0 +1,6 @@
+person = input("Enter your name here: ")
+
+if (person == "Kenobi"):
+    print("Hello there, you're a bold one Kenobi")
+else:
+    print("Hello there, you are not a bold one {person}")
