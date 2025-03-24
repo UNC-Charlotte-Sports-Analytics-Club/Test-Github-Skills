ca07696c834bb0c84464a2e7aae586f0d948956c
EddietheProgrammer <124279660+EddietheProgrammer@users.noreply.github.com>
2025-03-24 18:01:29 -0400

    Let a Niner Know

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Test-Github-Skills
 Feel free to mess with this repo and break it.
+
+**Hello World!!**
---
+++ main.py
@@ -0,0 +1,7 @@
+for i in range(100):
+    if i % 7 == 0:
+        print("Go Niners")
+    elif i % 4 == 0:
+        print("#LetaNinerKnow")
+    else:
+        print(i)
